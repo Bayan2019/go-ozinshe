1f214a7a17cda9de2ece2e6ff76ff1e7c7a9b4a3
Bayan2019 <saparbayevabt@gmail.com>
2025-02-19 05:49:36 +0500

    .gitignore

---
+++ views/json.go
@@ -0,0 +1,40 @@
+package views
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type ResponseMessage struct {
+	Message string `json:"message"`
+}
+
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
+
+func RespondWithError(w http.ResponseWriter, code int, msg string, err error) {
+	if err != nil {
+		log.Println(err)
+	}
+	if code > 499 {
+		log.Printf("Responding with 5XX error: %s", msg)
+	}
+
+	RespondWithJSON(w, code, ErrorResponse{
+		Error: msg,
+	})
+}
+
+func RespondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.WriteHeader(code)
+	w.Write(dat)
+}
