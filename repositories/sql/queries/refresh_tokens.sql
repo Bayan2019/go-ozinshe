05809d6956686e0228714a9413daf8799935f930
Bayan2019 <saparbayevabt@gmail.com>
2025-02-23 18:31:34 +0500

    [skip render]adding table refresh_tokens to migration and adding queries for refresh tokens

---
+++ repositories/sql/queries/refresh_tokens.sql
@@ -0,0 +1,24 @@
+-- name: CreateRefreshToken :one
+INSERT INTO refresh_tokens(token, created_at, updated_at, user_id, expires_at, revoked_at)
+VALUES (
+    ?, 
+    NOW(), NOW(), ?, 
+    NOW()+ INTERVAL '4 days', NULL
+)
+RETURNING token;
+--
+
+-- name: GetUserFromRefreshToken :one
+SELECT users.* FROM users
+JOIN refresh_tokens ON users.id = refresh_tokens.user_id
+WHERE refresh_tokens.token = ?
+    AND revoked_at IS NULL
+    AND expires_at > NOW()
+ORDER BY created_at DESC;
+--
+
+-- name: RevokeToken :exec
+UPDATE refresh_tokens
+SET updated_at = NOW(), revoked_at = NOW()
+WHERE token = ? AND user_id = ?;
+--
