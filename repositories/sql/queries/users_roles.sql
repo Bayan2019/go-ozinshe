20ea958ec37cdcb8bbe31fa3c239cb6e81d4ecda
Bayan2019 <saparbayevabt@gmail.com>
2025-02-23 07:37:25 +0500

    [skip render] adding query GetRolesOfUser

--- repositories/sql/queries/users_roles.sql
+++ repositories/sql/queries/users_roles.sql
@@ -3,6 +3,10 @@ INSERT INTO users_roles(user_id, role_id)
 VALUES (?, ?);
 --
 
+-- name: GetRolesOfUser :many
+SELECT * FROM users_roles WHERE user_id = ?;
+--
+
 -- name: RemoveRoleFromUser :exec
 DELETE FROM users_roles 
 WHERE user_id = ? AND role_id = ?;
