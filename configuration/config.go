68572e7a2d8342536e500fb8e663e340c02f313f
Bayan2019 <saparbayevabt@gmail.com>
2025-02-27 05:45:18 +0500

    working with environment variable

--- configuration/config.go
+++ configuration/config.go
@@ -13,8 +13,6 @@ import (
 // var MapCfg *MapConfiguration
 var ApiCfg *ApiConfiguration
 
-// type MapConfiguration struct{}
-
 type ApiConfiguration struct {
 	Conn      *sql.DB
 	DB        *database.Queries
