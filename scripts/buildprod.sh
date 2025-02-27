914ca152bbba226908a9ba4a2b446e53037420cd
Bayan2019 <saparbayevabt@gmail.com>
2025-02-27 06:00:02 +0500

    migrateUp.sh

--- scripts/buildprod.sh
+++ scripts/buildprod.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ozinshe-go
+# CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ozinshe-go
+GOOS=linux GOARCH=amd64 go build -o ozinshe-go
--- scripts/migrateup.sh
+++ scripts/migrateup.sh
@@ -4,5 +4,6 @@ if [ -f .env ]; then
     source .env
 fi
 
+
 cd repositories/sql/schema
 goose turso $DATABASE_URL up
